e6f2518d00eb6f7422764b9479da21f612cb4bdb
Tung Nguyen <tongueroo@gmail.com>
2018-01-18 14:59:10 -0800

    automatically fetch the latest launch template version when not specified by user

--- lib/aws_ec2/spot.rb
+++ lib/aws_ec2/spot.rb
@@ -24,12 +24,58 @@ module AwsEc2
 
     # params are main derived from profile files
     def params
-      load_profiles("spot/#{profile_name}").deep_symbolize_keys
+      params = load_profiles("spot/#{profile_name}")
+      params = decorate_launch_template_configs(params)
+      params.deep_symbolize_keys
+    end
+
+    # Decorates the launch_template_configs:
+    #
+    #   * Ensure that a launch template version is set
+    #   * Sets the ec2 tag name
+    def decorate_launch_template_configs(params)
+      launch_template_configs = params["spot_fleet_request_config"]["launch_template_configs"]
+      return params unless launch_template_configs
+
+      # Assume only one launch_template_configs
+      # TODO: add support for multiple launch_template_configs
+      config = launch_template_configs.first
+      spec = config["launch_template_specification"]
+      version = spec["version"]
+      unless version
+        latest_version = latest_version(spec["launch_template_name"])
+      end
+
+      config["launch_template_specification"]["version"] ||= latest_version
+
+      # TODO: figure out how to override a tag for a launch template
+      # # Sets the EC2 tag name.
+      # # Replace all tag_specifications for simplicity.
+      # tag_specifications = [{
+      #     resource_type: "instance",
+      #     tags: [{ key: "Name", value: @options[:name] }],
+      #   }]
+      # config["overrides"] << ["tag_specifications"] << tag_specifications
+
+      # replace all configs
+      params["spot_fleet_request_config"]["launch_template_configs"] = [config]
+
+      params
+    end
+
+    def latest_version(launch_template_name)
+      resp = ec2.describe_launch_template_versions(launch_template_name: launch_template_name)
+      version = resp.launch_template_versions.first
+      version.version_number.to_s # request_spot_fleet expects this to be a String
+    rescue Aws::EC2::Errors::InvalidLaunchTemplateNameNotFoundException => e
+      puts e.message
+      puts "Please double check that the launch template exists"
+      exit
     end
 
     def display_info
       puts "Using the following parameters:"
-      pp params
+      puts YAML.dump(params)
     end
   end
 end
