7533db2468d7155d6bfd4ec48a77d6772956f48f
Tung Nguyen <tongueroo@gmail.com>
2018-01-24 13:48:36 -0800

    fix ami creation command

--- lib/aws_ec2/scripts/ami_creation.sh
+++ lib/aws_ec2/scripts/ami_creation.sh
@@ -13,4 +13,5 @@ fi
 # Create AMI Bundle
 AMI_NAME="<%= @ami_name %>"
 INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 create-image --name $AMI_NAME --instance-id $INSTANCE_ID
+REGION=$(aws configure get region)
+aws ec2 create-image --name $AMI_NAME --instance-id $INSTANCE_ID --region $REGION
