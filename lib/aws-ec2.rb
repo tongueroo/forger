befa6d9978a2fafa0bf68a17177c203f0baaa395
Tung Nguyen <tongueroo@gmail.com>
2018-03-23 09:55:16 -0700

    require render_me_pretty as vendor submodule for now

--- lib/aws-ec2.rb
+++ lib/aws-ec2.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.expand_path("../", __FILE__))
 require "aws_ec2/version"
 require "colorize"
+
+$:.unshift(File.expand_path("../../vendor/render_me_pretty/lib", __FILE__))
 require "render_me_pretty"
 
 module AwsEc2
