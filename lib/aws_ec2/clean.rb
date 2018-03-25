216ed059fc707f22b544aa349687b12321e31e60
Tung Nguyen <tongueroo@gmail.com>
2018-03-24 22:06:43 -0700

    aws-ec2 clean ami command working

--- lib/aws_ec2/clean.rb
+++ lib/aws_ec2/clean.rb
@@ -4,6 +4,7 @@ module AwsEc2
   class Clean < Command
     desc "ami", "Clean until AMI available."
     long_desc Help.text("clean:ami")
+    option :keep, type: :numeric, default: 2, desc: "Number of images to keep"
     def ami(query)
       Cleaner::Ami.new(options.merge(query: query)).clean
     end
--- lib/aws_ec2/cleaner/ami.rb
+++ lib/aws_ec2/cleaner/ami.rb
@@ -3,7 +3,38 @@ module AwsEc2::Cleaner
     include AwsEc2::AwsService
 
     def clean
-      puts "Cleaning out old AMIs..."
+      query = @options[:query] + "*" # use query as base of name for search
+      keep = @options[:keep] || 2
+      puts "Cleaning out old AMIs with base name: #{@options[:query]}"
+      return if ENV['TEST']
+
+      images = search_ami(query)
+      images.sort_by! { |i| i.name }.reverse
+      delete_list = images[keep..-1] || []
+      puts "Deleting #{delete_list.size} images."
+      delete_list.each do |i|
+        puts "Deleting image: #{i.image_id} #{i.name}"
+        delete(i.image_id)
+      end
+    end
+
+  private
+    def delete(image_id)
+      ec2.deregister_image(image_id: image_id)
+    rescue Aws::EC2::Errors::InvalidAMIIDUnavailable
+      # happens when image was just deleted but its still
+      # showing up as available when calling describe_images
+      puts "WARN: #{e.message}"
+    end
+
+    def search_ami(query, owners=["self"])
+      ec2.describe_images(
+        owners: owners,
+        filters: [
+          {name: "name", values: [query]},
+          {name: "state", values: ["available"]}
+        ]
+      ).images
     end
   end
 end
