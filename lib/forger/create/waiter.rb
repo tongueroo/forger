9dc15eab31695b2440b8fa12710a7fdd143173fe
Tung Nguyen <tongueroo@gmail.com>
2018-04-28 09:45:08 -0700

    forger create --ssh option

--- lib/forger/create/waiter.rb
+++ lib/forger/create/waiter.rb
@@ -4,13 +4,11 @@ class Forger::Create
 
     def wait
       @instance_id = @options[:instance_id]
-      handle_wait
-      handle_ssh
+      handle_wait if @options[:wait]
+      handle_ssh if @options[:ssh]
     end
 
     def handle_wait
-      return unless @options[:wait]
-
       puts "Waiting for instance #{@instance_id} to be ready."
       # https://docs.aws.amazon.com/sdk-for-ruby/v3/developer-guide/using-waiters.html
       ec2.wait_until(:instance_running, instance_ids:[@instance_id]) do |w|
@@ -26,10 +24,44 @@ class Forger::Create
       puts "Instance #{@instance_id} is ready"
       dns = i.public_dns_name ? i.public_dns_name : 'nil'
       puts "Instance public_dns_name: #{dns}"
+      i
     end
 
     def handle_ssh
-      return unless @options[:ssh]
+      instance = handle_wait
+      unless instance.public_dns_name
+        puts "This instance does not have a public dns for ssh."
+        return
+      end
+
+      command = build_ssh_command(instance.public_dns_name)
+      puts "=> #{command.join(' ')}".colorize(:green)
+      retry_until_success(command)
+      Kernel.exec(*command) unless @options[:noop]
+    end
+
+    def build_ssh_command(host)
+      user = @options[:ssh_user] || "ec2-user"
+      [
+        "ssh",
+        ENV['SSH_OPTIONS'],
+        "#{user}@#{host}"
+      ].compact
+    end
+
+    def retry_until_success(*command)
+      retries = 0
+      uptime = command + ['uptime', '2>&1']
+      uptime = uptime.join(' ')
+      out = `#{uptime}`
+      while out !~ /load average/ do
+        puts "Can't ssh into the server yet.  Retrying until success." if retries == 0
+        print '.'
+        retries += 1
+        sleep 1
+        out = `#{uptime}`
+      end
+      puts ""
     end
   end
 end
--- lib/forger/help/create.md
+++ lib/forger/help/create.md
@@ -1,7 +1,17 @@
 Examples:
 
-    $ forger create my-instance
+    forger create my-instance
 
 To see the snippet of code that gets added to the user-data script you can use the `--noop` option and then view the generated tmp/user-data.txt.
 
-    $ forger create myscript --noop
+    forger create myscript --noop
+
+You can tell forger to wait until the instance is ready with the `--wait` option.
+
+    forger create my-instance --wait
+
+You can also tell forger to ssh into the instance immediately after it's ready with the `--ssh` option.  Examples:
+
+    forger create my-instance --ssh # default is to login as ec2-user
+    forger create my-instance --ssh --ssh-user ubuntu
+    SSH_OPTIONS "-A" forger create my-instance --ssh --ssh-user ubuntu
