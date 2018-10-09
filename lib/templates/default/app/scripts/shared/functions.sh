5809edcc8dc17c92532e9fb364cb079d8214ee17
Tung Nguyen <tongueroo@gmail.com>
2018-10-09 08:17:54 +0000

    configure_aws_cli: always write ~/.aws/config

--- lib/templates/default/app/scripts/shared/functions.sh
+++ lib/templates/default/app/scripts/shared/functions.sh
@@ -10,13 +10,11 @@ function configure_aws_cli() {
   local home_dir=$1
   # Configure aws cli in case it is not yet configured
   mkdir -p "$home_dir/.aws"
-  if [ ! -f "$home_dir/.aws/config" ]; then
-    EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-    EC2_REGION=${EC2_AVAIL_ZONE::-1}
-    cat >"$home_dir/.aws/config" <<CONFIGURE_AWS_CLI
+  EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+  EC2_REGION=${EC2_AVAIL_ZONE::-1}
+  cat >"$home_dir/.aws/config" <<CONFIGURE_AWS_CLI
 [default]
 region = $EC2_REGION
 output = json
 CONFIGURE_AWS_CLI
-  fi
 }
